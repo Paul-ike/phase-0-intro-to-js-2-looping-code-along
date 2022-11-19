95c3b3cbf39d3e368e258ac9e5fe5b03a719b609
Paul <paulnyoiken@gmail.com>
2022-11-19 14:09:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solutions in this file
+let spy;
+
+  beforeEach( () => {
+    spy = sinon.spy( console, 'log' );
+  } );
+
+  afterEach( () => {
+    spy.restore();
+  } );
+
+function writeCards(name, suprise) {
+    for (let i = 0; i < name.length; i++) {
+        name[i] = `Thank you, ${name[i]}, for the wonderful ${suprise} gift!`;
+    }
+    return name;
+}
+
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+
+function countDown(number) {
+    
+    while (number >= 0) {
+        console.log(number--);
+        debugger;
+    }
+}
+countDown(4)
